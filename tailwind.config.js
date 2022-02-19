729f8fc608bb9145d8c88f2ff7fe3507901e49a0
AndrewAlvaroh <andrewalvaroh@gmail.com>
2022-02-19 09:00:55 +0700

    set

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        fxs : '9px',
+        fsm : '12px',
+        fmd : '16px',
+        fbase : '20px',
+        flg : '24px',
+        fxl : '32px',
+        f2xl : '40px',
+        f3xl : '50px',
+      }
+    },
+  },
+  plugins: [],
+}
